8ac143a1f1d61a6a66ef963f0a2b27dfb7d6fe5c
slightlytwisted <hausauer@gmail.com>
2024-11-22 17:45:28 -0700

    Update textutils.ts
    Fixed typo.

--- src/textutils.ts
+++ src/textutils.ts
@@ -3,7 +3,7 @@ import { stripHtml } from "string-strip-html";
 
 // Helper functions for working with HTML.
 
-// Converts HTML for plain-text for use in full-text search, preview text, etc.
+// Converts HTML to plain-text for use in full-text search, preview text, etc.
 export function htmlToPlainText(html: string): string {
   // First, use he to resolve any HTML entities embedded in the text. Second, use string-strip-html to strip any HTML
   // tags so that we keep formatting tags out of the full-text search. Finally, replace newlines `\n` with spaces.
